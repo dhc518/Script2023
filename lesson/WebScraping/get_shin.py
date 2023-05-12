e639349601550e2f3ac35975f42f6afbcd5b3744
dhc518 <dhc000518@gmail.com>
2023-05-12 13:04:32 +0900

    add practice files

---
+++ lesson/WebScraping/get_shin.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+
+# f = open('alice.html')
+# soup = BeautifulSoup(f,'lxml')
+# f.close()
+#
+# for e in soup.find_all('a'):
+#     print(e['href'])
+
+import requests
+
+word = '신라면'
+url = f'https://www.google.com/search'
+params = {'q':word}
+headers = {
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+}
+
+r = requests.get(url, params=params, headers=headers)
+r.raise_for_status()
+
+soup = BeautifulSoup(r.text, 'lxml')
+for i, e in enumerate(soup.find_all('h3')):
+    print(f'#{i}: {e.string}')
+# with open('downloaded.html', 'w', encoding='utf-8') as wf:
+#     wf.write(r.text)
