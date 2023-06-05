d20c1bf5e2d06bdbf9c3922b03887d917e8ad032
dhc518 <dhc000518@gmail.com>
2023-06-05 22:07:28 +0900

    add project file

--- project/weather_subway/receive.py
+++ project/weather_subway/receive.py
@@ -11,12 +11,13 @@ def receive_message(msg):
     if 'text' in msg:
         text = msg['text']
         print(f'[{chat_title}:{chat_id}] {sender_fullname}({sender_username}) : {text}')
-        return text
+        return text, chat_id
     else:
         print(json.dumps(msg, indent=4, ensure_ascii=False))
 
 def get_updates():
     txt = None
+    chat_id = None
     # https://core.telegram.org/bots/api#getupdates
     url = f'https://api.telegram.org/bot{mybot.token}/getUpdates'
     r = requests.get(url)
@@ -26,7 +27,7 @@ def get_updates():
 
         for u in updates:
             if 'message' in u:
-                txt = receive_message(u['message'])
+                txt, chat_id = receive_message(u['message'])
             #print(json.dumps(u, indent=4, ensure_ascii=False))
         while updates:
             last_update_id = updates[-1]['update_id']
@@ -35,6 +36,6 @@ def get_updates():
                 updates = r.json()['result']
             else:
                 break
-        if not txt == None: return txt
+        if not txt == None: return txt, chat_id
     else:
         print(f'FAIL : {r.json()}')
--- project/weather_subway/send.py
+++ project/weather_subway/send.py
@@ -12,6 +12,3 @@ def send_text(chat_id, text):
     r = requests.get(url, params=params)
     print(json.dumps(r.json(), indent=4, ensure_ascii=False))
     assert(r.ok)
-
-send_text(-915250754, '안녕하세요 주인님 봇입니다.')
-send_text(-915250754, '공지사항입니다.')
